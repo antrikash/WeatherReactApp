adaef4664e7790ab27866771dacd6722736fe431
antrikash <antriksh.akshu@gmail.com>
2020-05-12 17:44:27 +0530

    Few css changes

--- src/Components/WeatherResult/index.js
+++ src/Components/WeatherResult/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 
 class WeatherResult extends Component {
   render() {
+    console.log("result", this.props.result.data);
     let date =
       this.props.result.data !== undefined
         ? new Date(this.props.result.data.dt).toDateString()
@@ -12,34 +13,49 @@ class WeatherResult extends Component {
       this.props.result.data !== undefined
         ? (this.props.result.data.main.temp - 273.1).toFixed(1)
         : "";
+    let maxTemp =
+      this.props.result.data !== undefined
+        ? (this.props.result.data.main.temp_max - 273.1).toFixed(1)
+        : "";
+    let minTemp =
+      this.props.result.data !== undefined
+        ? (this.props.result.data.main.temp_min - 273.1).toFixed(1)
+        : "";
     return (
       <Fragment>
-        <table>
-          <thead>
-            <tr>
-              <th>Location</th>
-              <th>Date</th>
-              <th>Temp in 'C </th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.props.result.data !== undefined && (
-              <tr>
-                <td>{this.props.result.data.name}</td>
-                <td>{date}</td>
-                <td>{temp}`c</td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-        {this.props.result.data !== undefined && (
-          <img
-            alt="icon"
-            src={`http://openweathermap.org/img/wn/${
-              this.props.result.data.weather[0].icon
-            }@2x.png`}
-          />
-        )}
+        <hr />
+        <div>
+          {this.props.result.data !== undefined && (
+            <img
+              alt="icon"
+              src={`http://openweathermap.org/img/wn/${
+                this.props.result.data.weather[0].icon
+              }@2x.png`}
+            />
+          )}
+          {this.props.result.data !== undefined && (
+            <h5>
+              {this.props.result.data.name},{" "}
+              {this.props.result.data.sys.country}
+            </h5>
+          )}
+          {this.props.result.data !== undefined && (
+            <div>
+              <span id="date">{date}</span>
+            </div>
+          )}
+          {this.props.result.data !== undefined && (
+            <span>{this.props.result.data.weather[0].description}</span>
+          )}
+          {this.props.result.data !== undefined && (
+            <div>
+              <span id="temp">{temp}°С</span>{" "}
+              <span id="text">
+                {`temperature from ${minTemp} to ${maxTemp}`}{" "}
+              </span>
+            </div>
+          )}
+        </div>
       </Fragment>
     );
   }
